5dbc31b090016d4d3c5cdaedd429e65d6eb9ca17
paulo56ser <paulo.fh@icloud.com>
2022-12-29 15:29:08 +0000

    29-12

--- hello.go
+++ hello.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá Mundo")
-	fmt.Println("Meu primeiro programa em Go!")
+	nome := "Paulo"
+	versao := 1.2
+	fmt.Println("Olá sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scanf("%d", &comando)
+
+	fmt.Println("O comando escolhindo foi", comando)
 }
